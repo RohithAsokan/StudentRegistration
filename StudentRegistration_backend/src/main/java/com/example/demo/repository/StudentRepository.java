c40040bdda1a5633223577312f33fd4cc5679c79
RohithAsokan <90841555+RohithAsokan@users.noreply.github.com>
2024-11-27 00:31:18 +0530

    Update StudentRepository.java

--- StudentRegistration_backend/src/main/java/com/example/demo/repository/StudentRepository.java
+++ StudentRegistration_backend/src/main/java/com/example/demo/repository/StudentRepository.java
@@ -1,9 +1,8 @@
 package com.example.demo.repository;
 
-import java.util.Optional;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import com.example.demo.model.Student;
 
 public interface StudentRepository extends MongoRepository<Student, String> {
-	Optional<Student> findById(String username);
+    Student findByUsername(String username);
 }
