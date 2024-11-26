f6eb0cece85fba148f6f39d3a1383fb422d1b042
RohithAsokan <90841555+RohithAsokan@users.noreply.github.com>
2024-11-27 00:30:41 +0530

    Update StudentService.java

--- StudentRegistration_backend/src/main/java/com/example/demo/service/StudentService.java
+++ StudentRegistration_backend/src/main/java/com/example/demo/service/StudentService.java
@@ -1,11 +1,15 @@
 package com.example.demo.service;
 
 import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.model.Student;
 import com.example.demo.repository.StudentRepository;
+import com.example.demo.service.exception.StudentAlreadyExistsException;
+import com.example.demo.service.exception.StudentNotFoundException;
+
 
 @Service
 public class StudentService {
@@ -13,31 +17,53 @@ public class StudentService {
     @Autowired
     private StudentRepository studentRepository;
 
-    public Student getStudentByUsername(String username) {
-    	return studentRepository.findById(username).orElse(null);
+    public boolean isUsernameAvailable(String username) {
+        Student student = studentRepository.findByUsername(username);
+        return student == null;
     }
 
-    public Student registerStudent(Student student) {
-        if (studentRepository.findById(student.getUsername()).isPresent()) {
-            throw new IllegalArgumentException("Username already exists!");
+    public String login(String username, String password) throws StudentNotFoundException {
+        Student existingStudent = studentRepository.findByUsername(username);
+        if (existingStudent == null || !existingStudent.getPassword().equals(password)) {
+            throw new StudentNotFoundException("Invalid username or password!");
         }
-        return studentRepository.save(student);
+        return existingStudent.getId();
     }
 
-    public Student updateStudent(Student student) {
-        return studentRepository.save(student);
+    public void registerStudent(Student student) throws StudentAlreadyExistsException {
+        if (!isUsernameAvailable(student.getUsername())) {
+            throw new StudentAlreadyExistsException("Username already exists!");
+        }
+        studentRepository.save(student);
     }
 
-    public Student updateStudentDetailsByUserName(String username, Student student) {
-        Optional<Student> existingStudentOpt = studentRepository.findById(username);
-        if (existingStudentOpt.isPresent()) {
-            Student existingStudent = existingStudentOpt.get();
-            existingStudent.setName(student.getName());
-            existingStudent.setPassword(student.getPassword());
-            existingStudent.setPhone(student.getPhone());
-            existingStudent.setEmail(student.getEmail());
-            return studentRepository.save(existingStudent); 
+    public Student getStudentById(String id) throws StudentNotFoundException {
+        Optional<Student> optionalStudent = studentRepository.findById(id);
+        if (optionalStudent.isPresent()) {
+            return optionalStudent.get();
         } 
-        else return null;
+        else {
+            throw new StudentNotFoundException("Student not found!");
+        }
+    }
+
+    public Student updateStudentDetailsByUserId(String userId, Student updatedStudent) throws StudentNotFoundException {
+        Optional<Student> optionalStudent = studentRepository.findById(userId);
+        if (optionalStudent.isPresent()) {
+            Student existingStudent = optionalStudent.get();
+            if (!existingStudent.getUsername().equals(updatedStudent.getUsername()) && !isUsernameAvailable(updatedStudent.getUsername())) {
+                throw new IllegalArgumentException("Username already exists!");
+            }
+            existingStudent.setUsername(updatedStudent.getUsername());
+            existingStudent.setPassword(updatedStudent.getPassword());
+            existingStudent.setName(updatedStudent.getName());
+            existingStudent.setPhone(updatedStudent.getPhone());
+            existingStudent.setEmail(updatedStudent.getEmail());
+
+            return studentRepository.save(existingStudent);
+        } 
+        else {
+            throw new StudentNotFoundException("Student not found!");
+        }
     }
 }
