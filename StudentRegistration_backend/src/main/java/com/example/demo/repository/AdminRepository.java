e9642ebc84b72c9f2a6c0ec0b21b6c58f9065341
RohithAsokan <90841555+RohithAsokan@users.noreply.github.com>
2024-12-11 08:48:28 +0530

    Delete StudentRegistration_backend/src/main/java/com/example/demo/repository/AdminRepository.java

--- StudentRegistration_backend/src/main/java/com/example/demo/repository/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.demo.model.Admin;
-
-public interface AdminRepository extends MongoRepository<Admin, String>{
-
-}
